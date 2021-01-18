41d4113f442f48cc650ed4d08a5d193820a4fa5e
Sasha <gsancho0001@gmail.com>
2021-01-18 18:23:50 +0200

    load testing mvp implemented (very slow)

--- src/loadTesting/loadTesting.go
+++ src/loadTesting/loadTesting.go
@@ -3,16 +3,49 @@ package loadTesting
 import (
 	"../configuration"
 	"fmt"
+	"log"
 	"net/http"
+	"sync"
+	"time"
 )
 
-func Run(config configuration.Configuration) {
-	fmt.Printf("Testing url -> %s \n", config.ServerUrl)
+const ATTEMPTS_COUNT = 100_000
 
-	resp, err := http.Get(config.ServerUrl)
-	if err != nil {
-		println("Got error -> ", err.Error())
-	} else {
-		println("Got response -> ", resp.Status)
+type Attempt struct {
+	IsSuccess bool
+	Error     string
+}
+
+type Report struct {
+	TotalTime time.Duration
+	Attempts  []Attempt
+}
+
+func Run(config configuration.Configuration) Report {
+	fmt.Printf("Testing url -> %s \n", config.ServerUrl)
+	var attempts [ATTEMPTS_COUNT]Attempt
+	var wg sync.WaitGroup
+	start := time.Now()
+	for i := 0; i < ATTEMPTS_COUNT; i++ {
+		wg.Add(1)
+		go SendRequest(config.ServerUrl, &attempts[i], &wg)
 	}
+	wg.Wait()
+	elapsed := time.Since(start)
+	print("\n")
+	log.Printf("Processed %d requests. Elapsed time %s", ATTEMPTS_COUNT, elapsed)
+	var report Report
+	report.Attempts = attempts[:]
+	report.TotalTime = elapsed
+	return report
+}
+
+func SendRequest(url string, result *Attempt, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer print(".")
+	_, err := http.Get(url)
+	if err != nil {
+		result.Error = err.Error()
+	}
+	result.IsSuccess = err == nil
 }
--- src/main.go
+++ src/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"./configuration"
 	"./loadTesting"
+	"encoding/json"
 	"flag"
+	"io/ioutil"
 )
 
 func main() {
 	configPtr := flag.String("configuration", "config.json", "a path to config file")
 	config := configuration.ReadFromFile(*configPtr)
 	flag.Parse()
-
-	loadTesting.Run(config)
+	report := loadTesting.Run(config)
+	bytes, err := json.Marshal(report)
+	if err == nil{
+		ioutil.WriteFile("./report.json", bytes, 0644)
+	}
 }
